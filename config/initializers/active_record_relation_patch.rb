8d87af75eba5e58ba7849637ae34a0fabcdb98d4
Jelani Woods <jelani@firstdraft.com>
2020-02-03 17:43:53 -0600

    Update .pryrc

--- config/initializers/active_record_relation_patch.rb
+++ config/initializers/active_record_relation_patch.rb
@@ -3,7 +3,9 @@ module ActiveRecord
     def to_s
       row_count = count
       
-      "Array containing #{row_count} #{model} #{"record".pluralize(row_count)}"
+      # "Array containing #{row_count} #{model} #{"record".pluralize(row_count)}"
+      "#{self.class} (array with #{row_count} #{model} instances inside)"
+
     end
   end
 end
