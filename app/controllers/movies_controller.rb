0e5ca5ebbef588ae98c369d800fd345cf0774abd
Raghu Betina <raghu@firstdraft.com>
2020-08-04 04:13:37 -0500

    Remove old movies_controller

--- app/controllers/movies_controller.rb
+++
@@ -1,9 +0,0 @@
-class MoviesController < ApplicationController
-  def casablanca
-    render({ :template => "movie_templates/cb.html.erb" })
-  end
-  
-  def before2k
-    render({ :template => "movie_templates/pre2000.html.erb" })
-  end
-end
