71ea29a15052ef18a5bb15697c79f68e9b41ac40
tvoutsos <tvoutsos@chicagobooth.edu>
2020-10-29 07:35:56 +0000

    First Commit

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,27 @@
+class DirectorsController < ApplicationController 
+
+  def index
+   render({ :template => "directors_template/index.html.erb" })
+  end
+
+  def wisest
+    @oldest = Director.where.not({ :dob => nil}).order({ :dob => :asc}).at(0)
+    render({ :template => "directors_template/eldest.html.erb" })
+  end
+
+  def dumb
+    @youngest = Director.where.not({ :dob => nil}).order({ :dob => :desc}).at(0)
+    render({ :template => "directors_template/youngest.html.erb" })
+  end
+
+  def director_details
+    # params looks like {"an_id"=>"42"}
+
+    the_id = params.fetch("an_id")
+
+    @the_director = Director.where({ :id => the_id}).at(0)
+
+    render({ :template => "directors_template/show.html.erb" })
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  
   get("/", { :controller => "application", :action => "homepage" })
+
+  get("/directors", { :controller => "directors", :action => "index"})
+
+  get("/directors/eldest", { :controller => "directors", :action => "wisest"})
+
+  get("/directors/youngest", { :controller => "directors", :action => "dumb"})
+
+  get("/directors/:an_id", { :controller => "directors", :action => "director_details" })
+  
 end
